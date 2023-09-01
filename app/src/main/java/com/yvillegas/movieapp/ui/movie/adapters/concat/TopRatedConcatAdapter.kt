de3467596bacceb0bad7f115b3725b49c151fdc9
yvillegas <villegasyanina.c@gmail.com>
2023-09-01 15:33:03 -0500

    Add - Config Top Rated Adapter

---
+++ app/src/main/java/com/yvillegas/movieapp/ui/movie/adapters/concat/TopRatedConcatAdapter.kt
@@ -0,0 +1,29 @@
+package com.yvillegas.movieapp.ui.movie.adapters.concat
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.yvillegas.movieapp.core.BaseConcatHolder
+import com.yvillegas.movieapp.databinding.TopRatedBinding
+import com.yvillegas.movieapp.ui.movie.adapters.MovieAdapter
+
+class TopRatedConcatAdapter(private val movieAdapter: MovieAdapter): RecyclerView.Adapter<BaseConcatHolder<*>>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseConcatHolder<*> {
+        val itemBinding = TopRatedBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ConcatViewHolder(itemBinding)
+    }
+
+    override fun getItemCount(): Int = 1
+
+    override fun onBindViewHolder(holder: BaseConcatHolder<*>, position: Int) {
+        when(holder){
+            is ConcatViewHolder -> holder.bind(movieAdapter)
+        }
+    }
+
+    private inner class ConcatViewHolder(val binding: TopRatedBinding): BaseConcatHolder<MovieAdapter>(binding.root){
+        override fun bind(adapter: MovieAdapter) {
+            binding.rvTopRated.adapter = adapter
+        }
+    }
+}
