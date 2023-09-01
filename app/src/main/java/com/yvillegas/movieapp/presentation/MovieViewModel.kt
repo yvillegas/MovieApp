359837a1e6b7fdfa18b86b26add1e32e16f94af4
yvillegas <villegasyanina.c@gmail.com>
2023-09-01 12:08:00 -0500

    Add MovieViewModel

---
+++ app/src/main/java/com/yvillegas/movieapp/presentation/MovieViewModel.kt
@@ -0,0 +1,26 @@
+package com.yvillegas.movieapp.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
+import com.yvillegas.movieapp.core.Resource
+import com.yvillegas.movieapp.domain.MovieRepository
+import kotlinx.coroutines.Dispatchers
+import java.lang.Exception
+
+class MovieViewModel(private val repository: MovieRepository): ViewModel() {
+    fun getMovies() = liveData(Dispatchers.IO) {
+        emit(Resource.Loading())
+        try {
+            emit(Resource.Success(Triple(repository.getUpcomingMovies(), repository.getTopRatedMovies() , repository.getPopularMovies())))
+        }catch (e: Exception){
+            emit(Resource.Failure(e))
+        }
+    }
+}
+
+class MovieViewModelFactory(private val repository: MovieRepository): ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(MovieRepository::class.java).newInstance(repository)
+    }
+}
