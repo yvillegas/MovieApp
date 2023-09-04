db5ba8a01ea10ae12ed941e644086ad77a04b953
yvillegas <villegasyanina.c@gmail.com>
2023-09-04 15:02:43 -0500

    Update localdatasource - favorite movie

--- app/src/main/java/com/yvillegas/movieapp/data/local/LocalMovieDataSource.kt
+++ app/src/main/java/com/yvillegas/movieapp/data/local/LocalMovieDataSource.kt
@@ -25,8 +25,11 @@ class LocalMovieDataSource(private val movieDao: MovieDao) {
         return movieDao.getAllMovies().filter { it.movieType == "favorite" }.toMovieList()
     }
 
-
-    suspend fun saveMovie(movie: MovieEntity){
-        movieDao.saveMovie(movie)
+    suspend fun addFavoriteMovies(id: String){
+        movieDao.addFavorite(id)
     }
+    suspend fun deleteFavoriteMovies(id: String){
+        movieDao.deleteFavorite(id)
+    }
+
 }
