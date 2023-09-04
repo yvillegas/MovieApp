93079462bef7ecc9b308c30316e72e403db4c3cc
yvillegas <villegasyanina.c@gmail.com>
2023-09-04 15:01:32 -0500

    Update movieDao - favorite movie

--- app/src/main/java/com/yvillegas/movieapp/data/local/MovieDao.kt
+++ app/src/main/java/com/yvillegas/movieapp/data/local/MovieDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.yvillegas.movieapp.data.model.entities.MovieEntity
 
 @Dao
@@ -13,4 +14,10 @@ interface MovieDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveMovie(movie: MovieEntity)
+
+    @Query("UPDATE MovieEntity SET favorite = 'true' WHERE id = :id")
+    suspend fun addFavorite(id: String)
+
+    @Query("UPDATE MovieEntity SET favorite = 'false' WHERE id = :id")
+    suspend fun deleteFavorite(id: String)
 }
